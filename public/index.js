4d9ef0daf092b9a26278a43d4ef3967eae9fb81b
blaze <blaze.vincent@hotmail.com>
2021-09-30 17:53:12 -0500

    Add basic file uploading functionality

--- public/index.js
+++ public/index.js
@@ -1,6 +1,46 @@
 const domElements = {
+    uploaderForm: document.querySelector('#uploader-form'),
     fileInput: document.querySelector('#file-input'),
-    uploadButton: document.querySelector('#upload-button')
+    submitInput: document.querySelector('#submit-input')
 }
 
+domElements.uploaderForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const fileSubmission = domElements.fileInput.files[0];
+    const fileName = fileSubmission.name.split('.');
+    fileName.splice(fileName.length - 1, 0, `-${Date.now()}.`);
+
+    const fileReader = new FileReader();
+    fileReader.onprogress = progress => {
+        console.log(`file loading-- ${Math.floor((progress.loaded / progress.total) * 100)}%`) //show loaded percentage
+    }
+    fileReader.onload = async _ => {
+        const CHUNK_SIZE = 50000; // bytes
+        const bufferData = _.target.result;
+
+        for(let chunkSector = 0; chunkSector < bufferData.byteLength; chunkSector += CHUNK_SIZE) 
+        {
+            const chunk = bufferData.slice(
+                chunkSector, 
+                //partition data into segments of less than or equal to CHUNK_SIZE
+                bufferData.byteLength - chunkSector > CHUNK_SIZE
+                    ? chunkSector + CHUNK_SIZE
+                    : chunkSector + bufferData.byteLength
+            )
+            await fetch('/upload', { //synchronous so data isn't out of order
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/octet-stream',
+                    'content-length': chunk.byteLength,
+                    'file-name': fileName.join('')
+                },
+                body: chunk
+            })
+        }
+    }
+
+    fileReader.readAsArrayBuffer(fileSubmission)
+
+})
 console.log(domElements);
--- server.js
+++ server.js
@@ -4,24 +4,59 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer();
-
 server.on('request', (req, res) => {
-    let destination;
-    switch(req.url){ //basic routing
-        case '/':
-            destination = 'public/index.html';
+    switch(req.method){
+        case 'GET':
+            let destination;
+            switch(req.url){ //basic routing
+                case '/':
+                    destination = 'public/index.html';
+                break;
+                default:
+                    destination = `public${req.url}`;
+                break;
+            }
+            try {
+                res.statusCode = 200;
+                res.end(fs.readFileSync(destination));
+            } catch(err){
+                res.statusCode = 404;
+                res.statusMessage = 'not found';
+                res.end()
+            }
         break;
-        default:
-            destination = `public${req.url}`;
+        case 'POST':
+            switch(req.url){
+                case '/upload':
+                    if(!req.headers['file-name']){
+                        res.statusCode = 400;
+                        res.statusMessage = 'request missing file-name header';
+                        res.end;
+                        break;
+                    }
+                    const filePath = `uploads/${req.headers['file-name']}`;
+
+                    if(fs.existsSync(filePath)){
+                        res.statusCode = 200;
+                    } else {
+                        res.statusCode = 201;
+                    }
+
+                    req.on('data', chunk => {
+                        fs.appendFileSync(filePath, chunk);
+                    })
+                    res.end('uploaded')
+                break;
+                default:
+                    res.statusCode = 401;
+                    res.statusMessage = 'POSTing not allowed on this route';
+                    res.end();
+                break;
+                
+            }
         break;
     }
-    try {
-        res.end(fs.readFileSync(destination));
-    } catch(err){
-        res.statusCode = 404;
-        res.statusMessage = 'not found';
-        res.end()
-    }
+
 });
 
 
